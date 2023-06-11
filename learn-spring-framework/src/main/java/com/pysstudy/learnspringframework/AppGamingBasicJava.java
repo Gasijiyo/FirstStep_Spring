fb5d7da58bcf9fe5a25cf44b4d8a6f4f0353b986
Gasijiyo <pys9307@naver.com>
2023-06-11 21:34:18 +0900

    .

--- learn-spring-framework/src/main/java/com/pysstudy/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/pysstudy/learnspringframework/AppGamingBasicJava.java
@@ -2,14 +2,22 @@ package com.pysstudy.learnspringframework;
 
 import com.pysstudy.learnspringframework.game.GameRunner;
 import com.pysstudy.learnspringframework.game.MarioGame;
+import com.pysstudy.learnspringframework.game.PacmanGame;
+import com.pysstudy.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 
-		var marioGame = new MarioGame();
-		var gameRunner = new GameRunner(marioGame);
+//		var marioGame = new MarioGame();
+//		var superContraGame = new SuperContraGame();
+		var pacmanGame = new PacmanGame();	// 1. Object Creation
+		
+		var gameRunner = new GameRunner(pacmanGame);
+		// 2. Object Creation + wiring of Dependencies
+		// Game is a Dependency of GameRunner
+		
 		gameRunner.run();
-	}
+	} 
 
 }
--- learn-spring-framework/src/main/java/com/pysstudy/learnspringframework/game/PacmanGame.java
+++ learn-spring-framework/src/main/java/com/pysstudy/learnspringframework/game/PacmanGame.java
@@ -1,5 +1,21 @@
 package com.pysstudy.learnspringframework.game;
 
-public class PacmanGame {
+public class PacmanGame implements GamingConsole{
+
+	public void up() {
+		System.out.println("up");
+	}
+	
+	public void down() {
+		System.out.println("down");
+	}
+	
+	public void left() {
+		System.out.println("left");
+	}
+	
+	public void right() {
+		System.out.println("right");
+	}
 
 }
