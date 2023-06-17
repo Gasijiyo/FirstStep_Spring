25d0748301776c0e26f36fc866666c755f138700
Gasijiyo <pys9307@naver.com>
2023-06-17 14:45:51 +0900

    .

--- learn-spring-framework/src/main/java/com/pysstudy/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/pysstudy/learnspringframework/HelloWorldConfiguration.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.Configuration;
 // Public accessor methods, constructor,
 // equals, hashcode and toString are automatically created.
 // Released in JDK16
-record Person (String name, int age) {};	
+record Person (String name, int age, Address address) {};	
 
-
+record Address (String firstLine, String city) {};
 
 @Configuration
 public class HelloWorldConfiguration {
@@ -20,7 +20,33 @@ public class HelloWorldConfiguration {
 	}
 	
 	@Bean
-	public Person person() {
-		return new Person("YS", 29);
+	public int age() {
+		return 99;
 	}
+	
+	@Bean
+	public Person person() {
+		return new Person("YS", 29, new Address("강남구", "서울시"));
+	}
+	
+	@Bean 
+	public Person person2MethodCall() {
+		return new Person("새이름", 100, address());
+	}
+	
+	@Bean 
+	public Person person3Parameters(String name, int age, Address 주소) {
+		return new Person(name, age, 주소);
+	}
+	
+	@Bean(name = "주소")
+	public Address address() {
+		return new Address ("응암동","서울특별시");
+	}
+	
+	@Bean(name = "address3")
+	public Address address3() {
+		return new Address ("교현동","충주시");
+	}
+	
 }
